6a356f6a443ed666191e30aeba562e9626db4ff6
Anurag Yadav <59284397+AnuragYadav171@users.noreply.github.com>
2020-01-14 00:38:16 +0530

    Update terraform_job.sh

--- terraform_job.sh
+++ terraform_job.sh
@@ -2,10 +2,10 @@
 whoami
 pwd
 which terraform
-aws s3 cp s3://shiraj001/terraform.tfstate /var/lib/jenkins/workspace/terraform-cd/
-aws s3 cp s3://shiraj001/shiraj.pem /var/lib/jenkins/workspace/terraform-cd/
-chmod 400 /var/lib/jenkins/workspace/terraform-cd/shiraj.pem
-cd /var/lib/jenkins/workspace/terraform-cd/
+aws s3 cp s3://shiraj001/terraform.tfstate .
+aws s3 cp s3://shiraj001/shiraj.pem .
+chmod 400 shiraj.pem
+# cd /var/lib/jenkins/workspace/terraform-cd/
 terraform init
 terraform plan
 which terraform
